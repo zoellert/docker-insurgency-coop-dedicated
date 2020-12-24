a290dd4bbba54ea83d591a01d2656abfc784a5aa
Til Zöller <til.zoeller@nordakademie.de>
2020-12-24 18:19:48 +0100

    Added workshop items

--- files/supervisor.sh
+++ files/supervisor.sh
@@ -26,6 +26,7 @@ install() {
     echo "Installing Insurgency Dedicated Server"
     /opt/steam/steamcmd.sh +login anonymous +force_install_dir /opt/steam/insurgency +app_update 237410 validate +quit
     cp -a /tmp/mapcycle_cooperative.txt /opt/steam/insurgency/insurgency/
+    cp -a /tmp/subscribed_file_ids.txt /opt/steam/insurgency/insurgency/
     cp -a /tmp/cfg/. /opt/steam/insurgency/insurgency/cfg/
     mkdir -p /opt/steam/insurgency/insurgency/scripts
     cp -a /tmp/scripts/. /opt/steam/insurgency/insurgency/scripts/
@@ -47,6 +48,6 @@ loadConfig
 echo "Starting Insurgency Dedicated Server"
 cd /opt/steam/insurgency
 export LD_LIBRARY_PATH=/opt/steam/insurgency:/opt/steam/insurgency/bin:${LD_LIBRARY_PATH}
-su steam -c "./srcds_linux -console +sv_lan 0 +servercfgfile server.cfg +map \"market hunt\" +maxplayers 48" & wait ${!}
+su steam -c "./srcds_linux -console +sv_lan 0 +servercfgfile server.cfg +map \"market hunt\" +maxplayers 48 -workshop" & wait ${!}
 echo "Insurgency Dedicated Server died"
 shutdown
